70c338cb57a368a927ab9bb38b5e242ab5a41404
Usuário <Usurio>
2017-07-28 16:38:04 -0300

    questao 2 hue

--- JavaScript/huebr.js
+++ JavaScript/huebr.js
@@ -4,3 +4,23 @@
 
 
 // QUESTÃO 2
+function troca(a) {
+    var bsim = document.getElementById("btn-sim");
+    var bnao = document.getElementById("btn-nao");
+    var aux;
+
+    if (a == bnao.id) {
+        aux = bsim.id;
+        bsim.id = bnao.id;
+        bnao.id = aux;
+
+        bsim.textContent = "Não";
+        bnao.textContent = "Sim";
+
+    }
+
+}
+
+function con() {
+   document.getElementById("pergunta").innerHTML= "Parabêns, você tem muito bom gosto!";
+}
