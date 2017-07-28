f5a91296011551194d5df0317067631924503ef4
higorc <higor_oliveira@outlook.com>
2017-07-28 15:35:16 -0400

    Questão 1, troca de questões implementada

--- JavaScript/huebr.js
+++ JavaScript/huebr.js
@@ -1,4 +1,44 @@
+//-----------------------------------------------------------------------------------------------------------------------------------
+//USÁVEIS
+var pergunta = document.querySelector("#pergunta");
+
+function adicionarClasse(classe) {
+    corpo.classList.toggle('frenetico');
+}
+
+function trocaQuestao(questaoAtual, proxQuestao) {
+    questaoAtual.classList.toggle("esconderDiv");
+    proxQuestao.classList.toggle("exibirDiv");
+}
+
+function trocaTextoPergunta(texto) {
+    pergunta.textContent = texto;
+}
+
+//-----------------------------------------------------------------------------------------------------------------------------------
 // QUESTÃO 1
+var btnSim = document.querySelector(".btn-sim");
+var btnNao = document.querySelector(".btn-nao");
+var corpo = document.querySelector("body");
+
+btnSim.addEventListener("click", function () {
+    setTimeout(function () {
+        trocaQuestao(document.querySelector(".q1"), document.querySelector(".q2"));
+    }, 1500);
+    trocaTextoPergunta("QUE BOM");
+    adicionarClasse("frenetico");
+})
+
+btnNao.addEventListener("click", function () {
+    setTimeout(() => {
+        trocaQuestao(2)
+    }, 1500);
+    trocaTextoPergunta("QUE PENA");
+    adicionarClasse("frenetico");
+})
+
+
+
 
 
 
